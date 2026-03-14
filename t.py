ef494572816cf70539ef61129a38661d677cb006
Ekaterina Leontieva <YOUR_GITHUB_EMAIL>
2026-03-14 21:30:32 +0000

    Initial commit for diabetes optimisation project

---
+++ t.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler # to normalise the data
+from sklearn.datasets import load_wine
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+print(1)

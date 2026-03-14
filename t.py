062553fd2e1b639c1361142848f03c6af6a72f67
Ekaterina Leontieva <YOUR_GITHUB_EMAIL>
2026-03-14 21:46:58 +0000

    Keep only diabets_optimisation notebook

--- t.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler # to normalise the data
-from sklearn.datasets import load_wine
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-print(1)
